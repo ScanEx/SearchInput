7236b229e713fa024790d534e7ce23551ed91fd1
liaozhai <laocan@yandex.ru>
2020-05-08 22:33:14 +0300

    Updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import css from 'rollup-plugin-css-porter';
 import pkg from './package.json';
-import copy from 'rollup-plugin-cpy';
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-porter';
+import cpy from 'rollup-plugin-cpy';
 
 export default [
     {
@@ -12,33 +12,43 @@ export default [
             file: pkg.browser,
             format: 'iife',
             name: 'SearchInput',
+            sourcemap: true,
         },
         plugins: [
-            resolve({ jsnext: true, main: false, module: true, browser: false }),
+            resolve(),
             commonjs(),
             css({minified: false, dest: 'dist/scanex-search-input.css'}),
-            copy({
+            cpy({
                 files: [ 'src/*.png' ],
                 dest: 'dist'            
             }),
-            babel(),
-        ],
+            babel({                
+                extensions: ['.js', '.mjs'],
+                exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'],
+                include: ['index.js', 'src/**']
+            }),
+	],
     },
     {
         input: 'index.js',
         output: { 
             file: pkg.module,
             format: 'cjs',
+            sourcemap: true,
         },
         plugins: [
-            resolve({ jsnext: true, main: false, module: true, browser: false }),
+            resolve(),
             commonjs(),
             css({minified: false, dest: 'dist/scanex-search-input.css'}),
-            copy({
+            cpy({
                 files: [ 'src/*.png' ],
                 dest: 'dist'            
             }),
-            babel(),
+            babel({                
+                extensions: ['.js', '.mjs'],
+                exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'],
+                include: ['index.js', 'src/**']
+            }),
         ],
     }
 ];
